00c9dc16833eda7c73a9e5118a4480da31cb89c5
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-08-08 00:31:13 +0545

    feat : add gmail

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = () => {
+menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+})
